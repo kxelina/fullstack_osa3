29e0594ef621e1256887e7153869c60cf88907e4
kxelina <elina88.k@gmail.com>
2024-07-19 20:53:21 +0300

    osa3:3.3

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {   id: "1",
+        name: 'Arto Hellas', 
+        number: '040-123456' },
+    {   id: "2",
+        name: 'Ada Lovelace', 
+        number: '39-44-5323523' },
+    {   id: "3",
+        name: 'Dan Abramov', 
+        number: '12-43-234345' },
+    {   id: "4",
+        name: 'Mary Poppendieck', 
+        number: '39-23-6423122' }
+]
+
+app.get('/api/persons', (request, response) => {
+if (persons) {
+    response.json(persons)
+  }else {
+    response.status(404).end()
+  }})
+
+app.get('/info', (request, response) => {
+const timestamp = new Date()
+if (persons) {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${timestamp.toString()}</p>`)
+    }else {
+    response.status(404).end()
+    }})
+
+ 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+if (person) {
+    response.json(person)
+    }else {
+    response.status(404).end()
+    }})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
+
